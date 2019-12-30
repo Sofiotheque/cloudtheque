cf6440af934e1f4abc94ecbb6e6e5e522f59937d
Sofiotheque <38701843+Sofiotheque@users.noreply.github.com>
2019-12-30 13:11:33 +0100

    Create ADS.js

---
+++ ADS.js
@@ -0,0 +1,2 @@
+//Display bas de page
+(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src="//61imk087ak.s.ad6media.fr/?d="+(new Date).getTime()+"&r=";try{a.src+=encodeURIComponent(top.document.referrer)}catch(c){a.src+=encodeURIComponent(document.referrer)}a.type="text/javascript";a.async=!0;b.parentNode.insertBefore(a,b)})();
